b1e00f83bc0e08595057708da96eea645aff65f4
margo <91476386+m-4rgo@users.noreply.github.com>
2025-05-23 07:41:05 +0100

    portfolio page setup- add pages system and sidebar links. populated with some content

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+
+// hide all sections except home
+const sections = document.querySelectorAll("section");
+for (const section of sections) {
+    if (section.id !== "home") {
+        section.classList.add("hidden");
+    }
+}
+
+// function to show/hide sections on navbar clicks
+function navToggle() {
+const navLinks = document.querySelectorAll("#navbar a");
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const sectionName = link.getAttribute('data-target').replace("#", "")
+        sections.forEach(section => section.classList.add("hidden"));
+
+        document.getElementById(sectionName).classList.remove("hidden");
+    });
+});
+}
+
+navToggle()
---
+++ todo.md
@@ -0,0 +1,4 @@
+set up automatic gallery add  
+create artwork pages  
+get page links working  
+set up pagination pages properly  
