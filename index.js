207036bcb5bf21e8c0c7237d12994fdc3125c17a
margo <91476386+m-4rgo@users.noreply.github.com>
2025-10-26 18:27:59 +0000

    commented out the image viewer code as buggy,image update

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ function burgerToggle() {
         })
     })
 }
-// call to function
-burgerToggle();
 
 // load artworks from JSON file
 function loadArtworks() {
@@ -48,8 +46,7 @@ function loadArtworks() {
             displayGallery(data)})
         .catch(error => console.error('failed to fetch data', error));
 }
-// call loadArtworks to fetch data from json file
-loadArtworks()
+
 
 // function to generate gallery from json data
 function displayGallery(data) {
@@ -79,9 +76,9 @@ function displayGallery(data) {
         // add gallery item to the right gallery
         document.getElementById(galleryId).appendChild(galleryItem);
 
-        // add click event listener to all images and call viewArtwork on click, so that images can be viewed fullscreen on click
+        // add click event listener to all images and call openImageViewer on click, so that images can be viewed fullscreen on click
         img.addEventListener("click", () => {
-            viewArtwork(img.src, img.dataset.title, img.dataset.description, artwork.section);
+            openImageViewer(img.src, img.dataset.title, img.dataset.description, artwork.section);
         })
     })
 }
@@ -126,56 +123,53 @@ function fadeIn(element) {
 
 // function to show/hide sections on navbar clicks
 function navToggle() {
-const navLinks = document.querySelectorAll("#navbar a");
+    const navLinks = document.querySelectorAll("#navbar a");
+    // cycle through nav a elements
+    navLinks.forEach(link => {
+        // add "on click" event listener for when each link is clicked
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
 
-// cycle through nav a elements
-navLinks.forEach(link => {
-    // add "on click" event listener for when each link is clicked
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
+            // remove the active class style for all links
+            navLinks.forEach(l => l.classList.remove("active"));
 
-        // remove the active class style for all links
-        navLinks.forEach(l => l.classList.remove("active"));
+            // add active style to clicked link
+            link.classList.add("active");
 
-        // add active style to clicked link
-        link.classList.add("active");
+            // get the target section from the link data
+            const sectionName = link.getAttribute('data-target').replace("#", "");
 
-        // get the target section from the link data
-        const sectionName = link.getAttribute('data-target').replace("#", "")
+            // redundant code as fadein/fadeout changed hiding technique
+            // sections.forEach(section => section.classList.add("hidden"));
 
-        // redundant code as fadein/fadeout changed hiding technique
-        // sections.forEach(section => section.classList.add("hidden"));
-
-        // set target section as the section name
-        const targetSection = document.getElementById(sectionName);
-        // if the target section is already visible, return
-        if (targetSection.style.display === "block") {
-            // reset scroll to top of page
-            window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-            return;
-        }
-
-        // cycle through sections and if section is target section, then fade in, else, fade out.
-        sections.forEach(section => {
-            if (section.id === sectionName) {
-                currentSectionName = sectionName;
-                currentSection = targetSection;
-                setTimeout(() => {
-                    fadeIn(section);
-                    window.scrollTo(0, 0);}, 300);
-            } else {
-                fadeOut(section);
-
+            // set target section as the section name
+            const targetSection = document.getElementById(sectionName);
+            // if the target section is already visible, return
+            if (targetSection.style.display === "block") {
+                // reset scroll to top of page
+                window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+                return;
             }
+
+            // cycle through sections and if section is target section, then fade in, else, fade out.
+            sections.forEach(section => {
+                if (section.id === sectionName) {
+                    currentSectionName = sectionName;
+                    currentSection = targetSection;
+                    setTimeout(() => {
+                        fadeIn(section);
+                        window.scrollTo(0, 0);}, 300);
+                } else {
+                    fadeOut(section);
+
+                }
+            });
         });
     });
-});
 }
-// call navToggle to allow navigation links to work.
-navToggle()
 
-// function to open artwork page on gallery image click
-function viewArtwork(src, title, description, section) {
+// function to open artwork page (image viewer) on gallery image click
+function openImageViewer(src, title, description, section) {
     // collect the scroll point of the page
     scrollPoint = window.scrollY;
     // hide the current section on image click
@@ -207,18 +201,71 @@ function viewArtwork(src, title, description, section) {
 // function to add functionality to image viewer close button
 function closeImageViewer() {
 // close image-viewer button functionality
-    document.getElementById("close-viewer").addEventListener("click", () => {
-        fadeOut(document.getElementById("image-viewer"));
-        document.getElementById("image-viewer").classList.add("viewer-hidden");
-        //     re-show the current section when close button is clicked
-        const section = document.getElementById(currentSectionName);
+    fadeOut(document.getElementById("image-viewer"));
+    document.getElementById("image-viewer").classList.add("viewer-hidden");
+    //     re-show the current section when close button is clicked
+    const section = document.getElementById(currentSectionName);
 
-
-        setTimeout(() => {
-            fadeIn(section);
-            window.scrollTo(0, scrollPoint);}, 300);
-
-    })
+    setTimeout(() => {
+        fadeIn(section);
+        window.scrollTo(0, scrollPoint);}, 300);
 }
-// call so close button works
-closeImageViewer();
+
+// function to add event listeners for image viewer controls
+function imageViewerControls() {
+    // add event listener for clicking back button
+    document.getElementById("close-viewer").addEventListener("click", () => {closeImageViewer()});
+    // add event listener for pressing down arrow
+    document.addEventListener("keydown", event => {
+        if (event.key === "ArrowDown") {
+            const viewerActive = document.getElementById("image-viewer");
+            if (viewerActive.style.display === "block") {
+                closeImageViewer();
+            }
+        }
+    });
+
+    // add event listener to image for click to fullscreen image
+    document.getElementById("viewer-img").addEventListener("click", () => {viewArtwork()});
+//     add event listener for up arrow to open fullscreen
+}
+
+//todo: this is buggy so commented out for live site
+// // function to toggle fullscreen view of artwork
+// function viewArtwork() {
+//
+//     const viewerImg = document.getElementById("viewer-img");
+//
+//     viewerImg.addEventListener("click", () => {
+//         // create overlay
+//         const overlay = document.createElement("div");
+//         overlay.classList.add("fullscreen");
+//
+//         // clone image
+//         const bigImg = document.createElement("img");
+//         bigImg.src = viewerImg.src;
+//         bigImg.alt = viewerImg.alt;
+//
+//         overlay.appendChild(bigImg);
+//         document.body.appendChild(overlay);
+//
+//         // click anywhere to exit
+//         overlay.addEventListener("click", () => {
+//             overlay.remove();
+//         });
+//     });
+// }
+
+// ensure all functions run after all page content is loaded.
+document.addEventListener("DOMContentLoaded", () => {
+    // call to function
+    burgerToggle();
+    // call loadArtworks to fetch data from json file
+    loadArtworks()
+    // call navToggle to allow navigation links to work.
+    navToggle()
+    // call so close button works
+    closeImageViewer();
+    // call this to add event listeners for the image viewer controls
+    imageViewerControls();
+})
--- todo.md
+++ todo.md
@@ -3,7 +3,10 @@
 ✅get page links working  
 ✅set up pagination pages properly  
 use up/down arrows to navigate menu  
+use left/right arrows to navigate art close-ups.
 create password protected secret upload page for uploading new artworks to the site. (a form that appends to the .json file.)  
 create secret upload page for adding updates to the home screen.  
-save scroll position in gallery when entering/exiting image viewer,  
-reset scroll position when switching between galleries.
+✅save scroll position in gallery when entering/exiting image viewer,  
+✅reset scroll position when switching between galleries.  
+"subscribe" page where you can subscribe to get SMS or email notifications when i post a new piece of work. port the page to a progressive web app so people can subscribe for push notifications too?  
+requests system where people can request an artwork.
